8ca22911532ccdcfe5bfa5b9da9b5409d278c070
Andy_SurfacePro_Kraken <phillipsaje@gmail.com>
2018-09-02 15:43:16 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cli_package',
-    version='0.0.2',
+    version='0.0.3',
     packages=['cli_package'],
     url='URL',
     license='License',
